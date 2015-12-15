39eb4b3cf9db71faee1bb272c472f7fc0e678576
Isaque Bressy <isaque.ikee@gmail.com>
2015-12-15 22:21:31 +0000

    Remove __get e __set do Model.

--- api/models/Model.php
+++ api/models/Model.php
@@ -9,16 +9,6 @@ class Model {
     protected $id;
     protected $db;
     protected $sql;
-    
-    public function __get($name)
-    {
-        return $this->$name;
-    }
-
-    public function __set($name, $value)
-    {
-        $this->$name = $value;
-    }
 
     public function __construct() {
         $this->db = Db::init();
